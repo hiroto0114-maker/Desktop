f0f3badb04b504e04eb90c0d0c3707ef592dd211
hiroto0114-maker <62985949+hiroto0114-maker@users.noreply.github.com>
2020-04-29 15:20:22 +0900

    123

---
+++ chat-space-master/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+# devise_for :users
+    root 'groups#index'   #ルートパスの指定
+ 	
+    resources :groups , only: [:new, :create, :edit, :update] do
+    	resources :messages, only: [:index, :create]
+    end
+    resources :users, only: [:edit, :update, :index] 
+end
